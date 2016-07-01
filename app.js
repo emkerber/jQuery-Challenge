7096adf1fc62b7929124fb84e815d4d2f1a76cf1
Liz Kerber <emkerber@outlook.com>
2016-07-01 15:40:08 -0500

    added comments to the JavaScript file and cleaned up a couple things

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function(){ //to initialize jQuery
 
-  var numOfButtons = 9;
-  var firstClick;
-  var secondClick;
+  var numOfButtons = 9; //set the number on the highest-value button
+  var firstClick; //initialize the variable so it's global
+  var secondClick; //initialize the variable so it's global
+  var clickCounter = 1; //this variable will check to see if the firstClick or the secondClick variable should store the value represented by the button clicked
 
-  function clickCounterChange() {
+  function clickCounterChange() { //changes the variable clickCounter between positive and negative
     clickCounter *= -1;
   };
 
-  function addNumbers(a, b) {
+  function addNumbers(a, b) { //the function that does the addition and concatenates the values into a string
     var c = a + b;
     console.log(a + " + " + b + " = " + c);
     return a + " + " + b + " = " + c;
   };
 
-  for (var i = 0; i <= numOfButtons; i++) {
+  for (var i = 0; i <= numOfButtons; i++) { //takes in the numOfButtons variable and appends the corresponding number of buttons to the DOM, while assigning each an ID that matches its value
     $("#buttons").append("<button id=\"" + i + "\">" + i + "</button>");
   }
 
-  var clickCounter = 1;
-
-  $("#buttons").on("click", "button", function() {
-    var tempButtonClick = parseInt($(this).attr("id"));
-    if (clickCounter > 0) {
+  $("#buttons").on("click", "button", function() { //click event
+    var tempButtonClick = parseInt($(this).attr("id")); //store the ID of the button clicked as a number
+    if (clickCounter > 0) { //if the clickCounter variable is positive, the value is assigned to the variable firstClick
       firstClick = tempButtonClick;
       console.log("firstClick = " + firstClick);
-      clickCounterChange();
+      clickCounterChange(); //make the clickCounter value negative
     } else {
-      secondClick = tempButtonClick;
+      secondClick = tempButtonClick; //otherwise the clickCounter variable is negative and the value is assigned to the secondClick varibale
       console.log("secondClick = " + secondClick);
-      clickCounterChange();
-      $("#answer").empty();
-      $("#answer").append(addNumbers(firstClick, secondClick));
+      clickCounterChange(); //make the clickCounter variable positive
+      $("#answer").empty(); //remove the answer text from the DOM after secondClick is assigned a (new) value
+      $("#answer").append(addNumbers(firstClick, secondClick)); //append a string to the DOM with the first and second buttons's values and sum
     }
   });
-
 
 })
